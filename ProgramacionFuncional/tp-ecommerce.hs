1cbd98815c60b47f2f2b6a07b7076388de1d2ea2
Carlos A. Vasquez <cvasquez@frba.utn.edu.ar>
2022-04-10 21:34:29 -0300

    Actualice todas las funciones a composiciones

--- ProgramacionFuncional/tp-ecommerce.hs
+++ ProgramacionFuncional/tp-ecommerce.hs
@@ -1,20 +1,27 @@
 --Funciones Dadas
+--Nota: abajo de tipar las funciones por mi mismo puse la verdadera declaración de cada función.
 --take :: Int -> String -> String
+--take :: forall a. Int -> [a] -> [a]
 
 --drop :: Int -> String -> String
+--drop :: forall a. Int -> [a] -> [a]
 
 --head :: String -> Char
+--head :: forall a. [a] -> a
 
 --elem :: Char -> String -> Bool
+--elem :: forall (t :: * -> *) a. (Foldable t, Eq a) => a -> t a -> Bool
 
 --reverse :: String -> String
-
+--reverse :: forall a. [a] -> [a]
 
 
 --Desarrollo
+--Aquí estoy muteando las sugerencias para que no me moleste VSCode
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 {-# HLINT ignore "Eta reduce" #-}
 {-# HLINT ignore "Redundant bracket" #-}
+{-# HLINT ignore "Use infix" #-}
 
 --Nota: podemos quitar la etiqueta "dia" y la función seguiría funcionando, pero considero que es poco expresivo.
 entregaSencilla :: String -> Bool
@@ -28,17 +35,21 @@ descodiciarProducto :: String -> String
 descodiciarProducto nombreProducto = take 10 nombreProducto 
 
 versionBarata :: String -> String
-versionBarata nombreProducto = reverse.descodiciarProducto $ nombreProducto
+versionBarata = reverse.descodiciarProducto
 
 
 productoDeLujo :: String -> Bool
-productoDeLujo nombreProducto = elem 'x' nombreProducto ||  elem 'z' nombreProducto
+productoDeLujo nombreProducto =  head nombreProducto `elem` ['x','z','X','Z']
 
 productoCodiciado :: String -> Bool
 productoCodiciado nombreProducto = length nombreProducto > 10
 
+esVocal :: Char -> Bool
+esVocal caracter = elem caracter ['a','e','i','o','u','A','E','I','O','U']
+
 productoCorriente :: String -> Bool
-productoCorriente nombreProducto = head nombreProducto == 'a' || head nombreProducto == 'e' || head nombreProducto == 'i' || head nombreProducto == 'o' || head nombreProducto == 'u'
+productoCorriente nombreProducto = esVocal.head $ nombreProducto
+
 
 productoDeElite :: String -> Bool
 productoDeElite nombreProducto = productoDeLujo nombreProducto && productoCodiciado nombreProducto && not (productoCorriente nombreProducto)
@@ -51,9 +62,11 @@ aplicarCostoDeEnvio :: Num a => a -> a -> a
 aplicarCostoDeEnvio precio costoEnvio = precio + costoEnvio
 
 
+multiplicarCantidad :: Num a => a -> a -> a
+multiplicarCantidad valor cantidad = valor * cantidad
+
 --Para hacer la composición debemos descomponer el código en funciones mas pequeñas
 precioTotal :: Fractional a => a -> a -> a -> a -> a
-precioTotal precioUnitario cantidad descuento costoEnvio = aplicarCostoDeEnvio ((aplicarDescuento precioUnitario descuento) * cantidad) costoEnvio
+precioTotal precioUnitario cantidad descuento costoEnvio = (aplicarCostoDeEnvio costoEnvio).(multiplicarCantidad cantidad).(aplicarDescuento precioUnitario) $ descuento
+--precioTotal precioUnitario cantidad descuento costoEnvio = aplicarCostoDeEnvio ((aplicarDescuento precioUnitario descuento) * cantidad) costoEnvio
 --Nota:" (aplicarDescuento precioUnitario descuento) * cantidad ".No necesita parentesis, pero considero que esto ayuda a leer un poco mejor el código
-
---Preguntar si podemos utilizar operadores lógicos && ||
